11aebc65fb7fa4c8ed3262e4a2f21544498985af
Duban-sg <universidad.duban@gmail.com>
2026-04-17 16:19:00 -0500

    to avoid damaging the original project

---
+++ README.md
@@ -0,0 +1,8 @@
+## Proyecto Tanquilandi-dots
+
+Este proyecto es un fork de [imperative-dots](https://github.com/ilyamiro/imperative-dots) con el objetivo de generar una personalización de instalación propia para `Arch Linux`. A partir de la base original, se adaptan configuraciones, temas y scripts para construir un entorno de escritorio completamente personalizado y reproducible.
+
+### One liner para probar
+```bash
+bash -c "$(curl -fsSL https://raw.githubusercontent.com/Duban-sg/tranquiland-dots/master/install.sh)"
+```
---
+++ modules/install_modules/install_process.sh
@@ -0,0 +1,196 @@
+
+
+
+#!/bin/bash
+
+source "./modules/install_modules/resolve_package.sh"
+source "./modules/install_modules/install_drivers.sh"
+source "./modules/install_modules/config_display_manager.sh"
+source "./modules/install_modules/config_wallpapers.sh"
+source "./modules/install_modules/copy_dotfiles.sh"
+source "./modules/install_modules/config_fonts.sh"
+source "./modules/install_modules/config_Adaptability.sh"
+source "./modules/install_modules/config_theme.sh"
+
+
+
+init_display() {
+    clear
+    draw_header
+    echo -e "\e[1;34m=== INSTALADOR IMPERATIVE-DOTS ===\e[0m"
+    echo "" # Espacio reservado para la barra de progreso (Línea 2)
+    echo "----------------------------------"
+}
+
+finish_display(){
+    echo -e "\n${BOLD}${C_MAGENTA}=== Installation Complete ===${RESET}\n"
+
+    if [ ${#FAILED_PKGS[@]} -ne 0 ]; then
+        echo -e "${BOLD}${C_RED}The following packages were NOT installed. Try building them yourself:${RESET}"
+        for fp in "${FAILED_PKGS[@]}"; do
+            echo -e "  - ${C_YELLOW}$fp${RESET}"
+        done
+        echo ""
+    fi
+
+    echo -e "Old configurations backed up to: ${C_CYAN}$BACKUP_DIR${RESET}"
+    echo -e "Please log out and log back in, or restart Hyprland to apply all changes."
+}
+
+# Función de progreso: uso -> mostrar_progreso actual total
+show_progress() {
+
+    local actual=$1
+    local total=$2
+    local ancho_barra=30
+    
+    # Cálculos
+    local porcentaje=$(( actual * 100 / total ))
+    local rellenos=$(( actual * ancho_barra / total ))
+    local vacios=$(( ancho_barra - rellenos ))
+
+    # Guardar posición actual del cursor
+    tput sc 
+
+    # Mover el cursor a la línea 2, columna 0 (donde está el espacio vacío)
+    tput cup 23 0 
+
+    # Construir y pintar la barra (con colores)
+    local barra_visual=$(printf "%${rellenos}s" | tr ' ' '#')
+    local vacio_visual=$(printf "%${vacios}s" | tr ' ' '-')
+    
+    # \e[K limpia la línea antes de escribir para que no queden restos
+    printf "\e[K\e[1;32mProgreso: [%s%s] %d%%\e[0m" "$barra_visual" "$vacio_visual" "$porcentaje"
+
+    # Restaurar el cursor a su posición original
+    tput rc
+}
+
+# Combine Base Packages with chosen Driver Packages
+combine_base_package(){
+    
+    ALL_PKGS=("${PKGS[@]}" "${DRIVER_PKGS[@]}")
+    MISSING_PKGS=()
+
+    echo -e "\n${C_CYAN}[ INFO ]${RESET} Checking for already installed packages..."
+    for pkg in "${ALL_PKGS[@]}"; do
+        # Skip empty entries if any
+        [[ -z "$pkg" ]] && continue 
+
+        # Check if package is installed locally
+        if pacman -Q "$pkg" &>/dev/null; then
+            true # Already installed, skip
+        else
+            MISSING_PKGS+=("$pkg")
+        fi
+    done
+}
+
+finalize_version_marker(){
+    
+    cat <<EOF > "$VERSION_FILE"
+LOCAL_VERSION="$DOTS_VERSION"
+WEATHER_API_KEY="$WEATHER_API_KEY"
+WEATHER_CITY_ID="$WEATHER_CITY_ID"
+WEATHER_UNIT="$WEATHER_UNIT"
+DRIVER_CHOICE="$DRIVER_CHOICE"
+KB_LAYOUTS="$KB_LAYOUTS"
+KB_LAYOUTS_DISPLAY="$KB_LAYOUTS_DISPLAY"
+KB_OPTIONS="$KB_OPTIONS"
+WALLPAPER_DIR="$WALLPAPER_DIR"
+EOF
+    printf "  -> Configuration and version state saved %-7s ${C_GREEN}[ OK ]${RESET}\n" ""
+}
+
+
+# ==============================================================================
+# Installation Process
+# ==============================================================================
+init_full_install() {
+
+    TOTAL_STEPS=11
+
+    init_display
+
+    show_progress 1 $TOTAL_STEPS
+    # Pre-authenticate sudo to prevent password prompts from breaking during piped commands
+    echo -e "${C_CYAN}[ INFO ]${RESET} Requesting sudo privileges for installation..."
+    sudo -v
+
+    # --- 2. Resolve Package Conflicts ---
+    show_progress 2 $TOTAL_STEPS
+    resolve_package
+
+    combine_base_package
+
+    # --- 3. Install Dependencies & Drivers ---
+    show_progress 3 $TOTAL_STEPS
+    install_drivers
+
+    # --- 4. Display Manager Cleanup & SDDM Setup ---
+    show_progress 4 $TOTAL_STEPS
+    config_display_manager
+
+    # --- 5. Repository Cloning & Wallpapers ---
+    show_progress 5 $TOTAL_STEPS
+    config_wallpapers
+
+    # --- 6. Copying Dotfiles & Backups ---
+    show_progress 6 $TOTAL_STEPS
+    copy_dotfiles
+
+    # --- 7. Fonts ---
+    show_progress 7 $TOTAL_STEPS
+    config_fonts
+
+    # --- 8. Adaptability Phase ---
+    show_progress 8 $TOTAL_STEPS
+    config_Adaptability
+
+    # --- 9. Setup SDDM Theme and Config ---
+    show_progress 9 $TOTAL_STEPS
+    config_theme
+
+    # --- 9. Finalize Version Marker & User State Persistence ---
+    show_progress 10 $TOTAL_STEPS
+    finalize_version_marker
+
+    # --- 9. Final Output ---
+    show_progress 11 $TOTAL_STEPS
+    finish_display
+
+}
+
+# ==============================================================================
+# Installation Process
+# ==============================================================================
+init_install_theme_and_config() {
+
+    TOTAL_STEPS=2
+
+    init_display
+
+    show_progress 1 $TOTAL_STEPS
+    # Pre-authenticate sudo to prevent password prompts from breaking during piped commands
+    echo -e "${C_CYAN}[ INFO ]${RESET} Requesting sudo privileges for installation..."
+    sudo -v
+
+    # --- 2. Repository Cloning & Wallpapers ---
+    show_progress 2 $TOTAL_STEPS
+    config_wallpapers
+
+    # --- 3. Copying Dotfiles & Backups ---
+    show_progress 3 $TOTAL_STEPS
+    copy_dotfiles
+
+    # --- 4. Fonts ---
+    show_progress 4 $TOTAL_STEPS
+    config_fonts
+
+    # --- 5. Setup SDDM Theme and Config ---
+    show_progress 5 $TOTAL_STEPS
+    config_theme
+
+    finish_display
+
+}
